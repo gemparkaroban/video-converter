96f8253c919cb3a5214cdf560a0acfe3f9c1321b
Max Li <maxlivinci@gmail.com>
2018-06-12 13:30:20 +0800

    wrapup on metadata

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ app.on('ready', () => {
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises).then(results => {
+    mainWindow.webContents.send('metadata:complete', results);
+  });
+});
