5cc5c4e5c327ba09eb9f76253eeff27c51d7529a
Max Li <maxlivinci@gmail.com>
2018-06-12 13:26:40 +0800

    receiving a List of Videos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { ipcRenderer } from 'electron';
 import {
   ADD_VIDEO,
   ADD_VIDEOS,
@@ -9,7 +10,12 @@ import {
 
 // TODO: Communicate to MainWindow process that videos
 // have been added and are pending conversion
-export const addVideos = videos => dispatch => {};
+export const addVideos = videos => dispatch => {
+  ipcRenderer.send('videos:added', videos);
+  ipcRenderer.on('metadata:complete', (event, videosWithData) => {
+    dispatch({ type: ADD_VIDEOS, payload: videosWithData });
+  });
+};
 
 // TODO: Communicate to MainWindow that the user wants
 // to start converting videos.  Also listen for feedback
